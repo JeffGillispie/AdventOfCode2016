ccdbcf41caf9fe8c740234821aaf723fe58f5402
JeffGillispie <email@jeffgillispie.com>
2017-01-28 16:56:29 -0600

    Completed the day 06 challenge.

--- AdventOfCode2016/Challenges/Day06.cs
+++ AdventOfCode2016/Challenges/Day06.cs
@@ -10,12 +10,34 @@ namespace AdventOfCode2016.Challenges
     {
         public string Part1(string input)
         {
-            return String.Empty;
+            string[] recordedMessages = System.IO.File.ReadAllLines(input);
+            var messageCharacters = recordedMessages
+                .First()                                                                                // get the first message
+                .Select((letter, index) => recordedMessages                                             // iterate through the characters of the first mesasge and grap the index
+                    .GroupBy(msg => msg[index])                                                         // group the characters by the column index
+                    .ToDictionary(groupedChar => groupedChar.Key, groupedChar => groupedChar.Count()))  // cast as a set of maps of char values and instance counts
+                .Select(frequencyMap => frequencyMap                                                    // iterate over the set of maps
+                    .OrderByDescending(col => col.Value)                                                // order the map in descending instance count order
+                    .First()                                                                            // get the most frequent instance
+                    .Key);                                                                              // return the character
+            string message = String.Join(String.Empty, messageCharacters);                              // join the message characters to a string
+            return message;
         }
 
         public string Part2(string input)
         {
-            return String.Empty;
+            string[] recordedMessages = System.IO.File.ReadAllLines(input);
+            var messageCharacters = recordedMessages
+                .First()                                                                                // get the first message
+                .Select((letter, index) => recordedMessages                                             // iterate through the characters of the first mesasge and grap the index
+                    .GroupBy(msg => msg[index])                                                         // group the characters by the column index
+                    .ToDictionary(groupedChar => groupedChar.Key, groupedChar => groupedChar.Count()))  // cast as a set of maps of char values and instance counts
+                .Select(frequencyMap => frequencyMap                                                    // iterate over the set of maps
+                    .OrderBy(col => col.Value)                                                          // order the map in instance count order
+                    .First()                                                                            // get the least frequent instance
+                    .Key);                                                                              // return the character
+            string message = String.Join(String.Empty, messageCharacters);                              // join the message characters to a string
+            return message;
         }
     }
 }
